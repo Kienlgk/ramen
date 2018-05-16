ac2a5f83d5ba1f6c29c43b74a9c1f1937baacbc0
kien <giakienlgk@gmail.com>
2018-05-17 03:10:00 +0700

    fix admin create error

--- server/src/jobs/db_init.ts
+++ server/src/jobs/db_init.ts
@@ -1,14 +1,23 @@
 import {models, sequelize} from "../models";
 
 module.exports = function () {
-    sequelize.sync({force: true}).then((rs) => {
-        rs.queryInterface.bulkInsert('Users', [{
-            username: 'admin',
-            password: 'd033e22ae348aeb5660fc2140aec35850c4da997',
-            isAdmin: true,
-            created_at: new Date(),
-            updated_at: new Date()
-        }], {});
+    sequelize.sync().then(async (rs) => {
+        let admin = await rs.model('User').findOne({
+            where: {
+                username: 'admin'
+            }
+        })
+        if(admin) {
+          console.log('admin is already created')
+        } else {
+          rs.queryInterface.bulkInsert('Users', [{
+              username: 'admin',
+              password: 'd033e22ae348aeb5660fc2140aec35850c4da997',
+              isAdmin: true,
+              created_at: new Date(),
+              updated_at: new Date()
+          }], {});
+        }
         console.log("Sync database successful");
     }).catch((e) => {
         console.log("Sync database failed: ", e.message);
